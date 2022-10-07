af8d3773e041a9c06415bd304032e48c7037158f
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-10-06 22:35:08 -0300

    Atualizando rota principal

--- src/router/principal_router.py
+++ src/router/principal_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
 # Minha API da rota principal
-rota_principal = APIRouter(prefix="",tags=["Principal",],)
+principal_route = APIRouter(prefix="",tags=["Principal",],)
 
 
-@rota_principal.get("/", response_model=str, summary="Diga oi.",
+@principal_route.get("/", response_model=str, summary="Diga oi.",
     description="Rota principal em que se diz um '`Oi`'.",)
 async def dizer_ola():
     return "Oi"
